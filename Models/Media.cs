5c9660f66fc9a1f66e90e0a0551cfb617f77120b
eivsch <you@example.com>
2026-05-27 10:36:37 +0200

    Switch to long for file sizes to avoid overflow for large files

--- Models/Media.cs
+++ Models/Media.cs
@@ -5,7 +5,7 @@ record Media
     public required string Id {get;set;}
     public required string Name {get;set;}
     public required DateTimeOffset Created {get;set;}
-    public int? Size {get;set;}
+    public long? Size {get;set;}
     public List<Tag> Tags {get;set;} = [];
     public int Likes {get;set;}
     public int? Index {get;set;}
--- Models/NewMediaRequest.cs
+++ Models/NewMediaRequest.cs
@@ -3,5 +3,5 @@ namespace MinimalGallery.API.Models;
 record NewMediaRequest
 {
     public required string Name {get;set;}
-    public int? Size {get;set;}
+    public long? Size {get;set;}
 }
